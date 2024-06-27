e0f211e50cc9d6ace9478b83186b0a2a6c8cec28
RidloGhifary <ridloghfry@gmail.com>
2024-06-27 09:04:49 +0700

    feat: Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-black">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}>
+          Auth
+        </h1>
+        <p className="text-lg text-white">A Simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant={"secondary"} size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }
