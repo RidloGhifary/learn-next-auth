470007de224514b760bf1435da8d80aaf3c94f93
RidloGhifary <ridloghfry@gmail.com>
2024-06-27 10:27:03 +0700

    feat: card wrapper

---
+++ app/auth/layout.tsx
@@ -0,0 +1,9 @@
+function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-full flex items-center justify-center bg-black">
+      {children}
+    </div>
+  );
+}
+
+export default AuthLayout;
